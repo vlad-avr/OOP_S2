d9189227a6f5c45b94a8103d8a9604ac75727fcd
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2024-03-19 13:32:55 +0200

    created test server with servlets (requires fix)

---
+++ src/main/java/servlets/CheckServlet.java
@@ -0,0 +1,27 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CheckServlet extends HttpServlet{
+	
+	public void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		String lb = req.getParameter("lb");
+		String rb = req.getParameter("rb");
+		String res;
+		if(rb.equals("yes") && lb.equals("yes")) {
+			res = "ball check passed";
+		}else if(rb.equals("no") && lb.equals("no")){
+			res = "Isufficient balls count -> ball check failed";
+		}else {
+			res = "Results uncertain -> provide more precise balls data";
+		}
+		
+		PrintWriter out = resp.getWriter();
+		out.println(res);
+	}
+}
